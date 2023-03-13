2cb124f40f8e19bf8ce0c833b4ec50ce4dd2be0d
shalini-lodhi <shalinilodhi01@gmail.com>
2023-03-13 21:25:24 +0530

    Add project files.

---
+++ SRP/Program.cs
@@ -0,0 +1,36 @@
+﻿using SRP;
+using System.Reflection.Metadata;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Ask for user informatin
+        Person person = new Person();
+
+        Console.WriteLine("Your first name: ");
+        person.FirstName = Console.ReadLine();
+
+        Console.WriteLine("Your Last Name: ");
+        person.LastName = Console.ReadLine();
+
+        //checks to be sure that first and last name are valid
+        if (string.IsNullOrWhiteSpace(person.FirstName))
+        {
+            Console.WriteLine("Not a valid first name");
+            Console.WriteLine("Correct first name: ");
+            person.FirstName = Console.ReadLine();
+            //return;
+        }
+        if (string.IsNullOrWhiteSpace(person.LastName))
+        {
+            Console.WriteLine("Not a valid last name");
+            Console.WriteLine("Write correct Last Name: ");
+            person.LastName = Console.ReadLine();
+            //return;
+        }
+
+        //Create a username for the person
+        Console.WriteLine($"User name is {person.FirstName} {person.LastName}");
+    }
+}
